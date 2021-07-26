36639775943c67c0e6c0a4bd5571fada6159cf64
Stephan Richter <s.richter@srsoftware.de>
2021-07-26 09:32:13 +0200

    implemented switching buses, gain and pano

--- src/main/java/de/srsoftware/midihub/M32C.java
+++ src/main/java/de/srsoftware/midihub/M32C.java
@@ -8,8 +8,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.List;
 import java.util.Vector;
 
 public class M32C implements Mixer {
@@ -18,13 +16,35 @@ public class M32C implements Mixer {
     private final OSCPortOut server;
     private de.srsoftware.midihub.Logger logger;
 
+    private static final int MAIN = 0;
+    private static final int BUS1 = 1;
+    private static final int BUS2 = 2;
+    private static final int BUS3 = 3;
+    private static final int BUS4 = 4;
+    private static final int BUS5 = 5;
+    private static final int BUS6 = 6;
+    private static final int BUS7 = 7;
+    private static final int BUS8 = 8;
+    private static final int BUS9 = 9;
+    private static final int BUS10 =10;
+    private static final int BUS11 =11;
+    private static final int BUS12 =12;
+    private static final int BUS13 =13;
+    private static final int BUS14 =14;
+    private static final int BUS15 =15;
+    private static final int BUS16 =16;
+
     private int offset=0;
-    private float [] mains = new float[CHANNELS];
+    private int bus = MAIN;
+    private float [][] channels = new float[17][CHANNELS];
+    private float[] gain = new float[CHANNELS];
+    private float[] pan = new float[CHANNELS];
 
     public M32C(String host, int port, de.srsoftware.midihub.Logger logger) throws IOException {
         this.logger = logger;
         server = new OSCPortOut(InetAddress.getByName(host),port);
         logger.log("Connected to {} @ {}:{}",getClass().getSimpleName(),host,port);
+        for (int i=0; i<CHANNELS;i++) pan[i] = 0.5f;
     }
 
     @Override
@@ -43,14 +63,46 @@ public class M32C implements Mixer {
 
     @Override
     public void changeMarker(int delta) {
+        unhighlightBus(bus);
+        bus+=delta;
+        if (bus> BUS16) bus = MAIN;
+        if (bus< MAIN) bus = BUS16;
+        highlightBus(bus);
+    }
 
+    private void unhighlightBus(int mix) {
+        try {
+            String channel = (mix < 10 ? "0" : "") + mix;
+            Vector<Integer> args = new Vector<>();
+            args.add(1);
+            OSCMessage message = new OSCMessage("/bus/"+channel+"/config/color", args);
+            server.send(message);
+            logger.log("sent OSC: {}  : {}",message.getAddress(),args);
+        } catch (IOException | OSCSerializeException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void highlightBus(int mix) {
+        try {
+            String channel = (mix < 10 ? "0" : "") + mix;
+            Vector<Integer> args = new Vector<>();
+            args.add(3);
+            OSCMessage message = new OSCMessage("/bus/"+channel+"/config/color", args);
+            server.send(message);
+            logger.log("sent OSC: {}  : {}",message.getAddress(),args);
+        } catch (IOException | OSCSerializeException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     public void changeTrack(int delta) {
         int count = Math.abs(delta);
         unhighlight(count);
-        offset = (offset+delta)%CHANNELS;
+        offset = (offset+delta);
+        if (offset+delta>CHANNELS) offset=0;
+        if (offset<0) offset = CHANNELS+delta;
         highlight(count);
     }
 
@@ -58,14 +110,12 @@ public class M32C implements Mixer {
         for (int i=offset+1; i<=offset+count; i++){
             try {
                 String channel = (i < 10 ? "0" : "") + i;
-                Vector<Integer> args = new Vector<Integer>();
+                Vector<Integer> args = new Vector<>();
                 args.add(9);
                 OSCMessage message = new OSCMessage("/ch/"+channel+"/config/color", args);
                 server.send(message);
                 logger.log("sent OSC: {}  : {}",message.getAddress(),args);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (OSCSerializeException e) {
+            } catch (IOException | OSCSerializeException e) {
                 e.printStackTrace();
             }
         }
@@ -75,14 +125,12 @@ public class M32C implements Mixer {
         for (int i=offset+1; i<=offset+count; i++){
             try {
                 String channel = (i < 10 ? "0" : "") + i;
-                Vector<Integer> args = new Vector<Integer>();
+                Vector<Integer> args = new Vector<>();
                 args.add(8);
                 OSCMessage message = new OSCMessage("/ch/"+channel+"/config/color", args);
                 server.send(message);
                 logger.log("sent OSC: {}  : {}",message.getAddress(),args);
-            } catch (IOException e) {
-                e.printStackTrace();
-            } catch (OSCSerializeException e) {
+            } catch (IOException | OSCSerializeException e) {
                 e.printStackTrace();
             }
         }
@@ -99,9 +147,7 @@ public class M32C implements Mixer {
             OSCMessage message = new OSCMessage("/-stat/solosw/"+channel, args);
             server.send(message);
             logger.log("sent OSC: {}  : {}",message.getAddress(),enabled?"ON":"OFF");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (OSCSerializeException e) {
+        } catch (IOException | OSCSerializeException e) {
             e.printStackTrace();
         }
     }
@@ -122,32 +168,65 @@ public class M32C implements Mixer {
             OSCMessage message = new OSCMessage("/ch/"+channel+"/mix/on", args);
             server.send(message);
             logger.log("sent OSC: {}  : {}",message.getAddress(),enabled?"ON":"OFF");
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (OSCSerializeException e) {
+        } catch (IOException | OSCSerializeException e) {
             e.printStackTrace();
         }
     }
 
     @Override
     public void handlePoti(int num, float percent) {
+        if (bus == MAIN) handleGain(num,percent); else handlePano(num,percent);
+
+    }
+
+    private void handlePano(int num, float percent) {
         try {
 
             num += offset;
 
             float new_val = percent / 100;
+            float old_val = pan[num-1];
+
+            if (Math.abs(new_val - old_val)>0.02) return;
+            pan[num-1] = new_val;
 
             String channel = (num < 10 ? "0" : "") + num;
 
             Vector<Object> args = new Vector<>();
             args.add(new_val);
 
-            OSCMessage message = new OSCMessage("/ch/"+channel+"/preamp/trim", args);
+            String address = bus == MAIN ? "/ch/" + channel + "/preamp/trim" : "/ch/" + channel + "/mix/pan";
+
+            OSCMessage message = new OSCMessage(address, args);
             server.send(message);
-            logger.log("sent OSC: {}  : {}",message.getAddress(),new_val);
-        } catch (IOException e) {
+            logger.log("sent OSC: {}  : {}",address,new_val);
+        } catch (IOException | OSCSerializeException e) {
             e.printStackTrace();
-        } catch (OSCSerializeException e) {
+        }
+    }
+
+    private void handleGain(int num, float percent) {
+        try {
+
+            num += offset;
+
+            float new_val = percent / 100;
+            float old_val = gain[num-1];
+
+            if (Math.abs(new_val - old_val)>0.02) return;
+            gain[num-1] = new_val;
+
+            String channel = (num < 10 ? "0" : "") + num;
+
+            Vector<Object> args = new Vector<>();
+            args.add(new_val);
+
+            String address = bus == MAIN ? "/ch/" + channel + "/preamp/trim" : "/ch/" + channel + "/mix/pan";
+
+            OSCMessage message = new OSCMessage(address, args);
+            server.send(message);
+            logger.log("sent OSC: {}  : {}",address,new_val);
+        } catch (IOException | OSCSerializeException e) {
             e.printStackTrace();
         }
     }
@@ -158,23 +237,23 @@ public class M32C implements Mixer {
         num += offset;
 
         float new_val = percent / 100;
-        float old_val = mains[num-1];
+        float old_val = channels[bus][num-1];
 
         if (Math.abs(new_val - old_val)>0.02) return;
-        mains[num-1] = new_val;
+        channels[bus][num-1] = new_val;
 
         String channel = (num < 10 ? "0" : "") + num;
+        String mixbus = (bus < 10 ? "0" : "") + bus;
 
         Vector<Object> args = new Vector<>();
         args.add(new_val);
 
-        OSCMessage message = new OSCMessage("/ch/"+channel+"/mix/fader", args);
+        String address = bus == MAIN ? "/ch/"+channel+"/mix/fader" : "/ch/"+channel+"/mix/"+mixbus+"/level";
+        OSCMessage message = new OSCMessage(address, args);
         try {
             server.send(message);
             logger.log("sent OSC: {}  : {}",message.getAddress(),args);
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (OSCSerializeException e) {
+        } catch (IOException | OSCSerializeException e) {
             e.printStackTrace();
         }
     }
